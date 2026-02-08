4126daa7ca75f0f38dcad40f3e4c6aa857ce1259
cilxry <cilxry@outlook.com>
2026-02-08 17:55:39 +0800

    刚那不对

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="答题系统 API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:4321","http://8.159.156.167:80"],  # Astro 默认端口
-    # allow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
